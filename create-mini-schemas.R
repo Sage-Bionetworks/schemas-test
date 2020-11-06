31951393b7b0df3a689ebd72142f88b21eb78dd9
Kara Woo <kara.woo@sagebase.org>
2020-11-06 11:37:08 -0800

    Add all the annotation terms, and include module name in JSON Schema

--- create-mini-schemas.R
+++ create-mini-schemas.R
@@ -4,37 +4,54 @@
 
 ## Create individual schemas for each key in experimentalData
 
+library("tools")
 library("jsonlite")
 library("here")
 library("tidyverse")
+library("fs")
 
 exp <- fromJSON(
   here("source-schemas", "experimentalData.json"),
   simplifyDataFrame = FALSE
 )
 
-create_mini_schema <- function(data, key) {
+create_mini_schema <- function(data, key, module) {
   schema <- c(
     "$schema" = "http://json-schema.org/draft-07/schema#",
-    "$id" = glue::glue("karatestorg20201105-{key}.json-0.0.1"),
+    "$id" = glue::glue("karatestorg20201105-{module}.{key}.json-0.0.1"),
     data
   )
   prettify(toJSON(schema, auto_unbox = TRUE))
 }
 
-json_versions <- imap(
-  exp$definitions,
-  ~ create_mini_schema(data = .x, key = .y)
-)
+json_file_to_mini_schemas <- function(file) {
+  dat <- fromJSON(file, simplifyDataFrame = FALSE)
+  module <- basename(file_path_sans_ext(file))
+  imap(dat$definitions, ~ create_mini_schema(data = .x, key = .y, module = module))
+}
 
-iwalk(
-  json_versions,
-  ~ write(
-    .x,
-    file = here(
-      "terms",
-      "experimentalData",
-      glue::glue("{.y}.json")
+source_files <- dir_ls(here("source-schemas"))
+names(source_files) <- basename(file_path_sans_ext(source_files))
+
+## Create JSON Schema terms for each module
+json_versions <- map(source_files, ~ json_file_to_mini_schemas(file = .x))
+
+## Ensure output folders exist
+dir_create(here("terms", names(source_files)))
+
+write_files_from_module <- function(data, module_name) {
+  iwalk(
+    data,
+    ~ write(
+      .x,
+      file = here(
+        "terms",
+        module_name,
+        glue::glue("{.y}.json")
+      )
     )
   )
-)
+}
+
+## Write out all JSON mini-schemas to appropriate module subfolders
+iwalk(json_versions, ~ write_files_from_module(data = .x, module_name = .y))
